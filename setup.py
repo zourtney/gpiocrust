783cddc40976e83720b78ca196ced654b0cbb1ec
zourtney <zourtney@gmail.com>
2014-03-11 01:02:28 -0500

    Build and CI files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='gpiocrust',
+      version='0.1',
+      description='A pythonic wrapper around RPi.GPIO',
+      author='zourtney',
+      author_email='zourtney@gmail.com',
+      url='https://github.com/zourtney/gpiocrust',
+      package_dir={'': 'gpiocrust'},
+      packages=['']
+     )
